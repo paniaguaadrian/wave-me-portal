8aa1f2e373c5bf1e4c0e18e2e1b2ef021a67c147
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2022-08-31 19:00:20 +0200

    Add form to write a message and populate all the waves from users

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
-import './App.css';
+
 import abi from './utils/WavePortal.json';
+import './App.css';
 
 export default function App() {
     const [currentAccount, setCurrentAccount] = useState('');
     const [waveStatus, setWaveStatus] = useState('');
-    const [totalWaves, setTotalWaves] = useState('');
+    const [totalWavesNum, setTotalWavesNum] = useState('');
+    const [allWaves, setAllWaves] = useState([]);
+    const [userMessage, setUserMessage] = useState('');
 
     const contractAddress = '0x9C447D3CBA17D0c000d73714b7893839B28da765';
     const contractABI = abi.abi;
@@ -16,20 +19,18 @@ export default function App() {
             const { ethereum } = window;
 
             if (!ethereum) {
-                console.log('Make sure you have metamask');
-            } else {
-                console.log('We have the ethereum object', ethereum);
+                window.alert('Make sure you have metamask');
             }
+
             const accounts = await ethereum.request({ method: 'eth_accounts' });
 
             if (accounts.length !== 0) {
-                const account = accounts[0];
-                console.log('Found an aouthorized account: ', account);
+                getAllWaves();
             } else {
-                console.log('No authorized account found');
+                window.alert('No authorized account found');
             }
         } catch (error) {
-            console.error(error);
+            window.alert(error);
         }
     };
 
@@ -38,7 +39,9 @@ export default function App() {
             const { ethereum } = window;
 
             if (!ethereum) {
-                console.log('Install Metamask!');
+                window.alert(
+                    'Please nstall Metamask in order to connect your Wallet'
+                );
                 return;
             }
 
@@ -46,7 +49,6 @@ export default function App() {
                 method: 'eth_requestAccounts',
             });
 
-            console.log('Connected: ', accounts);
             setCurrentAccount(accounts[0]);
         } catch (error) {
             console.error(error);
@@ -66,14 +68,51 @@ export default function App() {
                 );
 
                 let count = await wavePortalContract.getTotalWaves();
-                await setTotalWaves(count.toNumber());
+                await setTotalWavesNum(count.toNumber());
             }
         } catch (error) {
-            console.log(error);
+            window.alert(error);
+        }
+    };
+
+    const getAllWaves = async () => {
+        try {
+            const { ethereum } = window;
+            if (ethereum) {
+                const provider = await new ethers.providers.Web3Provider(
+                    ethereum
+                );
+                const signer = provider.getSigner();
+                const wavePortalContract = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer
+                );
+
+                const waves = await wavePortalContract.getAllWaves();
+
+                let wavesCleaned = [];
+                waves.forEach((wave) => {
+                    wavesCleaned.push({
+                        address: wave.waver,
+                        timestamp: new Date(wave.timestamp * 1000),
+                        message: wave.message,
+                    });
+                });
+                setAllWaves(wavesCleaned);
+            } else {
+                window.alert("Ethereum object doesn't exist");
+            }
+        } catch (error) {
+            window.alert(error);
         }
     };
 
     const wave = async () => {
+        if (!userMessage) {
+            window.alert("You should write me something cool isn't it? :D");
+            return;
+        }
         if (currentAccount !== '') {
             try {
                 const { ethereum } = window;
@@ -89,35 +128,43 @@ export default function App() {
                         signer
                     );
 
-                    const waveTxn = await wavePortalContract.wave();
-                    console.log('Mining...', waveTxn.hash);
+                    const waveTxn = await wavePortalContract.wave(userMessage);
                     setWaveStatus('mining');
 
                     await waveTxn.wait();
-                    console.log('Mined -- ', waveTxn.hash);
                     setWaveStatus('minted');
 
                     let count = await wavePortalContract.getTotalWaves();
-                    await setTotalWaves(count.toNumber());
+                    await setTotalWavesNum(count.toNumber());
 
                     setInterval(() => {
                         setWaveStatus('');
                     }, 5000);
                 } else {
-                    console.log("Ethereum object doesn't exist!");
+                    window.alert("Ethereum object doesn't exist!");
                 }
             } catch (error) {
-                console.error(error);
+                window.alert(error);
             }
         } else {
             window.alert('Connect your Metamask Wallet before Wave!');
         }
     };
 
+    const onChange = (event) => {
+        setUserMessage(event.target.value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setUserMessage('');
+    };
+
     useEffect(() => {
         checkIfWalletIsConnected();
         getTotalWavesNumber();
-    }, [totalWaves]);
+        // eslint-disable-next-line
+    }, [totalWavesNum]);
 
     return (
         <div className="mainContainer">
@@ -134,14 +181,26 @@ export default function App() {
                     space trying to jump to web3 space. Connect your wallet and
                     wave at me!
                 </div>
-
-                <button className="waveButton" onClick={wave}>
-                    {waveStatus === 'mining'
-                        ? 'Mining your wave...'
-                        : waveStatus === 'minted'
-                        ? 'Minted Wave!!'
-                        : 'Wave at Me'}
-                </button>
+                <div className="bio">
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            value={userMessage}
+                            onChange={onChange}
+                        />
+                        <button
+                            className="waveButton"
+                            onClick={wave}
+                            type="submit"
+                        >
+                            {waveStatus === 'mining'
+                                ? 'Mining your wave...'
+                                : waveStatus === 'minted'
+                                ? 'Minted Wave!!'
+                                : 'Wave at Me'}
+                        </button>
+                    </form>
+                </div>
 
                 {!currentAccount && (
                     <button className="waveButton" onClick={connectWallet}>
@@ -149,7 +208,28 @@ export default function App() {
                     </button>
                 )}
 
-                <div className="bio">Total waves to Adrian: {totalWaves}</div>
+                <div className="bio">
+                    Total waves to Adrian: {totalWavesNum}
+                </div>
+
+                {allWaves.map((wave, index) => {
+                    return (
+                        <div
+                            key={index}
+                            style={{
+                                backgroundColor: 'OldLace',
+                                marginTop: '16px',
+                                padding: '8px',
+                            }}
+                        >
+                            <div>Address: {wave.address}</div>
+                            <div>
+                                Time: {wave.timestamp.toString().slice(0, 29)}
+                            </div>
+                            <div>Message: {wave.message}</div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
